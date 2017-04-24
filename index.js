80b44b7b5f7313bd8f1dbd55ef76ca05786e4024
yding <yding@webmd.net>
2017-04-24 16:50:44 -0400

    fix options

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
   "rules": {
-    "font-family-no-duplicate-names": "error",
-    "string-no-newline": "error",
-    "declaration-block-no-duplicate-properties": "error",
-    "block-no-empty": "error",
-    "comment-no-empty": "error",
-    "no-duplicate-selectors": "error",
-    "no-extra-semicolons": "error",
-    "no-unknown-animations": "error"
+    "font-family-no-duplicate-names": true,
+    "string-no-newline": true,
+    "declaration-block-no-duplicate-properties": true,
+    "block-no-empty": true,
+    "comment-no-empty": true,
+    "no-duplicate-selectors": true,
+    "no-extra-semicolons": true,
+    "no-unknown-animations": true
   }
 };
